ba40b20bb820336c03ee22aac8b7cd15d92d5d48
grshankersai <grshankersai@gmail.com>
2024-10-01 18:22:28 +0530

    validations-lint-issues

--- validations/validations.go
+++ validations/validations.go
@@ -9,12 +9,12 @@ import (
 func ValidateExpenseObject(expense *entity.Expense) error{
 	
 	if expense.Amount <= 0 {		
-		return errors.New("Amount should be greater than zero")
+		return errors.New("amount should be greater than zero")
 	}
 
 	
 	if expense.Category == "" {		
-		return errors.New("Amount should be greater than zero")
+		return errors.New("amount should be greater than zero")
 	}
 
 	return nil
